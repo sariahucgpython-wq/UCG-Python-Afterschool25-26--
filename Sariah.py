b74bf687ef8a719190678a84ae41a4ef612321ee
sariahucgpython-wq <sariahucgpython@gmail.com>
2025-12-02 18:46:25 -0600

    Delete Sariah.py

--- Sariah.py
+++
@@ -1,5 +0,0 @@
-bill = float(input("Enter bill amount: "))
-people=int(input("Enter Howmanypeople amount:"))
-tax_rate=1.0825
-bill_after=str((bill*tax_rate/people))
-print("your bill is "+ bill_after)
